c3ba2b66d3f6a269ba56dd7088f121abdddb84e1
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-20 01:47:07 -0500

    Create algorithm test file

---
+++ algorithm/test.py
@@ -0,0 +1,19 @@
+def canBeSplitted(array):
+    """
+
+    :param array: array of elements to which the check will be applied
+    :return:
+    """
+
+    flag = 0
+
+    if array:
+        flag = 1
+
+    return flag
+
+
+if __name__ == '__main__':
+    # matrix = [1, 3, 3, 8, 4, 3, 2, 3, 3]
+    matrix = []
+    print(canBeSplitted(matrix))
