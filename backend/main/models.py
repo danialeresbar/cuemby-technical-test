827ed90cd7c785c255d54eb2a49a23c59e193be2
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-20 17:38:04 -0500

    Add player and team models

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Team model constants
+TEAM_NAME = 'name'
+TEAM_VERBOSE_NAME = 'Team'
+TEAM_VERBOSE_NAME_PLURAL = 'Teams'
+
+# Player model constants
+PLAYER_NAME = 'name'
+PLAYER_LAST_NAME = 'last_name'
+PLAYER_NATIONALITY = 'nationality'
+PLAYER_POSITION = 'position'
+PLAYER_TEAM = 'team'
+
+
+class Team(models.Model):
+    """
+
+    """
+
+    name = models.CharField(max_length=128, verbose_name=TEAM_NAME)
+
+    class Meta:
+        verbose_name = TEAM_VERBOSE_NAME
+        verbose_name_plural = TEAM_VERBOSE_NAME_PLURAL
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Player(models.Model):
+    """
+
+    """
+
+    name = models.CharField(max_length=64, verbose_name=PLAYER_NAME)
+    last_name = models.CharField(max_length=64, verbose_name=PLAYER_LAST_NAME)
+    nationality = models.CharField(max_length=64, verbose_name=PLAYER_NATIONALITY, blank=True)
+    position = models.CharField(max_length=32, verbose_name=PLAYER_POSITION, blank=True)
+    team = models.ForeignKey(Team, verbose_name=PLAYER_TEAM, on_delete=models.deletion.CASCADE)
+
+    class Meta:
+        verbose_name = 'Player'
+        verbose_name_plural = 'Players'
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
