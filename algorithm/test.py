214355a8b1daa9cd3d1527d24e78d4858cb34718
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-20 02:33:45 -0500

    Complete split test

--- algorithm/test.py
+++ algorithm/test.py
@@ -2,18 +2,23 @@ def canBeSplitted(array):
     """
 
     :param array: array of elements to which the check will be applied
-    :return:
+    :return: Integer which indicates if the array can be split
     """
 
-    flag = 0
+    if not array:
+        return 0
 
-    if array:
-        flag = 1
+    for index, _ in enumerate(array):
+        if index > 0:
+            left_slice = array[:index]
+            right_slice = array[index:]
+            # print(f'Left slice: {left_slice} ------ Right slice: {right_slice}')
+            if sum(left_slice) == sum(right_slice):
+                return 1
 
-    return flag
+    return -1
 
 
 if __name__ == '__main__':
-    # matrix = [1, 3, 3, 8, 4, 3, 2, 3, 3]
-    matrix = []
+    matrix = [1, 3, 3, 8, 4, 3, 2, 3, 3]
     print(canBeSplitted(matrix))
