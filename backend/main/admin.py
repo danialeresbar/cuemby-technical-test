a684e02750ba6ee6ac42b54bdec865c1448de839
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-20 17:38:55 -0500

    Add player and team model admin

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, Team
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Player', {
+            'fields': (('name', 'last_name'), 'nationality')
+        }),
+        ('Club', {
+            'fields': ('team', 'position')
+        })
+    )
+
+    list_display = ('id', 'name', 'last_name', 'nationality', 'team')
+    list_editable = ('name', 'last_name', 'nationality', 'team')
+    list_filter = ('name', 'last_name', 'team')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Team', {
+            'fields': ('name',)
+        }),
+    )
+
+    list_display = ('id', 'name')
+    list_editable = ('name', )
