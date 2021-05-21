06b894e4a651dc3668abfdf787bf8a19c4542d94
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-21 03:05:15 -0500

    Add api urls

--- backend/cuemby/urls.py
+++ backend/cuemby/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from main.urls import apiurls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(apiurls))
 ]
 
 if settings.DEBUG:
