e8c3756dd0a5e48b99725ca727ca2b5781003e3c
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-21 08:23:43 -0500

    Add filtering feature

--- backend/main/api/views/player_views.py
+++ backend/main/api/views/player_views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, filters
+from rest_framework import generics
+from main.api.filters import NameFilterBackend, OrderFilterBackend
 from main.api.serializers import PlayerSerializer
 from main.api.pagination import CustomPagination
 
@@ -7,11 +8,15 @@ class PlayerListView(generics.ListAPIView):
     pagination_class = CustomPagination
     serializer_class = PlayerSerializer
     queryset = serializer_class.Meta.model.objects.all().order_by('name')
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('name', )
+    filter_backends = (NameFilterBackend, OrderFilterBackend)
 
     def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(self.get_queryset(), many=True)
+        filters = request.query_params
+        if filters:
+            search = filters.get('search', '')
+            order = filters.get('order', 'asc')
+            page = filters.get('page', 1)
+        serializer = self.serializer_class(self.filter_queryset(self.get_queryset()), many=True)
         page = self.paginate_queryset(serializer.data)
         return self.get_paginated_response(page)
 
