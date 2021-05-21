f6c46294ac1abf597ecf6bc0da0333f9c04c2b1f
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-21 07:18:34 -0500

    Update apiurls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 
 from main.api.views.player_views import PlayerListView
-from main.api.views.team_views import TeamListView
+from main.api.views.team_views import TeamView
 
 
 apiurls = [
     path('players', PlayerListView.as_view()),
-    path('teams', TeamListView.as_view())
+    path('team/', TeamView.as_view())
 ]
