7a9caeb83f036511b92e927b0ee953a9d811546f
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-21 06:55:43 -0500

    Add pagination feature

--- backend/main/api/views/team_views.py
+++ backend/main/api/views/team_views.py
@@ -1,9 +1,19 @@
-from rest_framework import generics, filters
-from main.api.serializers import TeamSerializer
+from rest_framework import response, views
+from main.api.serializers import PlayerSerializer, Team, TeamSerializer
+from main.api.pagination import PaginationHandlerMixin, CustomPagination
 
 
-class TeamListView(generics.ListAPIView):
-    serializer_class = TeamSerializer
-    queryset = serializer_class.Meta.model.objects.all()
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('name', )
+class TeamView(views.APIView, PaginationHandlerMixin):
+    pagination_class = CustomPagination
+
+    def post(self, request):
+        name = request.data.get('Name', '')
+        team = Team.objects.filter(name__icontains=name).order_by('name').first()
+        players = PlayerSerializer.Meta.model.objects.filter(team=team).order_by('name')
+        page = self.paginate_queryset(players)
+        if page is not None:
+            serializer = self.get_paginated_response(PlayerSerializer(page, many=True).data)
+        else:
+            serializer = PlayerSerializer(page)
+        data =serializer.data
+        return response.Response(data)
