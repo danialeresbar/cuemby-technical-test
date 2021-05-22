81c019365355e4aab516e3fd2b32d8609c651dd8
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-21 20:19:02 -0500

    Create custom management command

--- backend/main/management/commands/populate_database.py
+++ backend/main/management/commands/populate_database.py
@@ -1,44 +1,74 @@
-#!/usr/bin/env python
-
-import django
-import os
 import requests
-import sys
-
-sys.path.append('/home/cuembyadm/app')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'cuemby.settings.development'
-django.setup()
+from django.core.management import BaseCommand
 
 from main.models import Player, Team
 
+FUT21_BASE_URL = 'https://www.easports.com/fifa/ultimate-team/api/fut/item?page='
 
-def populate_database(players):
-    for player in players:
-        name = f"{player.get('firstName')} {player.get('lastName')}"
-        nation = player.get('nation', dict()).get('name')
-        position = player.get('position', '')
-        team = player.get('club', dict()).get('name')
-        team_object, _ = Team.objects.get_or_create(name=team)
-        Player.objects.get_or_create(
-            name=name,
-            nation=nation,
-            position=position,
-            team=team_object
+
+class Command(BaseCommand):
+    help = 'Fetch data from fut21 API to populate the project DB'
+    teams = list()
+
+    @staticmethod
+    def get_data():
+        page = 1
+        teams = list()
+        players = list()
+
+        while True:
+            print(f'Fetching page: {page}')
+            url = f'{FUT21_BASE_URL}{page}'
+            page += 1
+
+            try:
+                response = requests.get(url).json()
+            except:
+                print(f'Data for page {page} cannot be fetched')
+                break
+
+            for player in response.get('items', list()):
+                team = player.get('club', dict()).get('name')
+
+                players.append({
+                    'name': f"{player.get('firstName')} {player.get('lastName')}",
+                    'nation': player.get('nation', dict()).get('name'),
+                    'position': player.get('position', ''),
+                    'team': team
+                })
+
+                teams.append(team)
+
+                if page > response.get('totalPages'):
+                    break
+        print('Fetch finished')
+
+        teams_filtered = [{'name': team} for team in set(teams)]
+        return players, teams_filtered
+
+    def save_teams(self, teams):
+        print('Saving teams found')
+        self.teams = Team.objects.bulk_create(
+            [Team(**team) for team in teams]
         )
 
+    def save_players(self, players):
+        print('Saving players found')
+        # Initially, we need to map the team for any player
+        for player in players:
+            player['team'] = self.get_club_id_from_name(player['team'])
 
-if __name__ == '__main__':
-    url = f'https://www.easports.com/fifa/ultimate-team/api/fut/item?page='
-    fut_request = requests.get(url=url + '1')
+        Player.objects.bulk_create(
+            [Player(**player) for player in players if player['team']]
+        )
 
-    try:
-        response = fut_request.json()
-        populate_database(response.get('items', list()))
-        total_pages = response.get('totalPages', 1)
-        for page in range(2, total_pages + 1):
-            fut_request = requests.get(url=f'{url}{page}')
-            response = fut_request.json()
-            populate_database(response.get('items', list()))
-    except Exception:
-        response = fut_request.text
-        print('Error')
+    def get_club_id_from_name(self, name):
+        for team in self.teams:
+            if team.name == name:
+                return team
+
+    def handle(self, *args, **kwargs):
+        players, teams = self.get_data()
+        self.save_teams(teams)
+        self.save_players(players)
+
