a2c77093c7592011d05d547da4ddac9dd7bee550
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-22 00:40:13 -0500

    Fix page filtering

--- backend/main/api/views/team_views.py
+++ backend/main/api/views/team_views.py
@@ -8,23 +8,15 @@ class TeamView(views.APIView, PaginationHandlerMixin):
     pagination_class = CustomPagination
     permission_classes = (CustomPermission, )
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         self.check_permissions(request)
         name = request.data.get('Name', '')
-        page = request.data.get('Page', 1)
         team = Team.objects.filter(name__icontains=name).order_by('name').first()
         players = PlayerSerializer.Meta.model.objects.filter(team=team).order_by('name')
-        page = self.paginate_queryset(players)
+        page = self.paginate_queryset(players, request)
         if page is not None:
             serializer = self.get_paginated_response(PlayerSerializer(page, many=True).data)
         else:
             serializer = PlayerSerializer(page)
         data =serializer.data
         return response.Response(data)
-
-
-class TeamRetrieveView(generics.GenericAPIView):
-    pagination_class = CustomPagination
-
-    def post(self, request, *args, **kwargs):
-        pass
