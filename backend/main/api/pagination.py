1099abcb563118efb64f161137ba24067402c58e
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-22 01:55:44 -0500

    Remove pagination mixin

--- backend/main/api/pagination.py
+++ backend/main/api/pagination.py
@@ -1,30 +1,6 @@
 from rest_framework import response, pagination
 
 
-class PaginationHandlerMixin:
-
-    @property
-    def paginator(self):
-        if not hasattr(self, '_paginator'):
-            if self.pagination_class is None:
-                self._paginator = None
-            else:
-                self._paginator = self.pagination_class()
-        else:
-            pass
-        return self._paginator
-
-    def paginate_queryset(self, queryset, request, view=None):
-
-        if self.paginator is None:
-            return None
-        return self.paginator.paginate_queryset(queryset, request, view=self)
-
-    def get_paginated_response(self, data):
-        assert self.paginator is not None
-        return self.paginator.get_paginated_response(data)
-
-
 class CustomPagination(pagination.PageNumberPagination):
     """
     Pagination subclass for custom GET and POST request pagination
